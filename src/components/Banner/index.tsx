78c3fea987d95e9f72e24953f5faea4c53913539
Thay Mendes <thay.gm@live.com>
2024-03-03 15:17:20 -0300

    banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,16 +1,24 @@
 import { Image, Title, Prices } from "./styles";
 
-import bannerImg from "../../assets/images/banner-homem-aranha.png";
+import bannerImg from "../../assets/images/banner-elden-ring.png";
+import Tag from "../Tag";
+import Button from "../../Button";
 
 const Banner = () => (
   <Image style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
-      <Title>Marvel&apos;s Spider-Man: Miles Morales PC</Title>
-      <Prices>
-        De <span> R$ 259,99</span>
-        <br />
-        por apenas R$ 99,90
-      </Prices>
+      <Tag size="big">Destaque do dia</Tag>
+      <div>
+        <Title>Elden Ring: Shadow of Erdtree PC</Title>
+        <Prices>
+          De <span> R$ 259,99</span>
+          <br />
+          por apenas R$ 99,90
+        </Prices>
+      </div>
+      <Button type="link" to="/produto" title="Clique aqui para aproveitar!">
+        Aproveitar
+      </Button>
     </div>
   </Image>
 );
